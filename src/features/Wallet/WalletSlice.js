c4a283a941fddf029559cc9467a55588209fe389
Jayesh Harlalka <f20210873@pilani.bits-pilani.ac.in>
2024-09-18 14:43:03 +0530

    Updated Dashboard

--- src/features/Wallet/WalletSlice.js
+++ src/features/Wallet/WalletSlice.js
@@ -1,32 +1,32 @@
-// src/features/Wallet/WalletSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
 export const walletSlice = createSlice({
-  name: 'wallet',
-  initialState: {
-    isConnected: false,
-    address: null,
-    balance: {},
-    transactions: [],
-  },
-  reducers: {
-    connectWallet: (state, action) => {
-      state.isConnected = true;
-      state.address = action.payload.address;
-      // Add more initial states as needed
+    name: 'wallet',
+    initialState: {
+        address: '',
+        balance: 0,
+        profitLoss: 0,
+        isConnected: false,
     },
-    disconnectWallet: (state) => {
-      state.isConnected = false;
-      state.address = null;
-      state.balance = {};
-      state.transactions = [];
+    reducers: {
+        connectWallet: (state, action) => {
+            // Assuming action.payload contains { address, balance, profitLoss }
+            state.address = action.payload.address;
+            state.balance = action.payload.balance;
+            state.profitLoss = action.payload.profitLoss;
+            state.isConnected = true; // Set isConnected to true when wallet is connected
+        },
+        removeWallet: (state) => {
+            state.address = '';
+            state.balance = 0;
+            state.profitLoss = 0;
+            state.isConnected = false; // Set isConnected to false when wallet is removed
+        },
     },
-    // Add additional reducers for other wallet interactions if necessary
-  },
 });
 
-// Export the actions
-export const { connectWallet, disconnectWallet } = walletSlice.actions;
+// Destructure and export the actions
+export const { connectWallet, removeWallet } = walletSlice.actions;
 
-// Export the reducer
+// Default export the reducer
 export default walletSlice.reducer;
